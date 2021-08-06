5e9d0c2d43122828f64e82a079739859d3ced6d4
Jack-rm <jackcnsr94@gmail.com>
2021-08-06 16:58:56 +0200

    Mail and roll of the dice exercise completed

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,6 +1,25 @@
+// Esercizio database email 
+
 let emailUser = prompt("Inserisci la tua e-mail");
 let listaDiEmail = ["jimmy123@gmai.com", "pincopallothebest@gmail.com", "atticidicittà@gmail.com", "mazinga69@gmail.com", "banana1994@gmail.com"];
 
-let presente = listaDiEmail.includes(emailUser);
+let presenza = listaDiEmail.includes(emailUser);
 
-document.getElementById("email_user").innerHTML = presente;
+document.getElementById("email_user").innerHTML = presenza;
+
+
+// Minigioco del tiro di dadi
+
+let randomNumberPlayer = parseInt( (6 * Math.random())) +1;
+let randomNumberMachine = parseInt( (6 * Math.random())) +1;
+
+document.getElementById("random_player").innerHTML = randomNumberPlayer;
+document.getElementById("random_machine").innerHTML = randomNumberMachine;
+
+if (randomNumberPlayer === randomNumberMachine){
+    document.getElementById("risultato").innerHTML = "Pareggio!";
+}   else if (randomNumberPlayer > randomNumberMachine){
+        document.getElementById("risultato").innerHTML = "il Giocatore!  :)";
+}   else{
+        document.getElementById("risultato").innerHTML = "il Computer!  :(";
+}
